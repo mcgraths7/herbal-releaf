a060b3085f886a788bb55ab2818199b6dac6a2ad
Steven McGrath <mcgrath.steven7@gmail.com>
2022-04-10 11:40:18 -0400

    added basic layout

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import Header from '../Header';
 import Main from '../Main';
 import Footer from '../Footer';
+import Nav from '../Nav';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
       <Main />
       <Footer />
     </div>
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="flex jst-even container">
+      <a href="#" className="page-link text-md text-bold">
+        Home
+      </a>
+      <a href="#" className="page-link text-md text-bold">
+        Categories
+      </a>
+      <a href="#" className="page-link text-md text-bold">
+        Herbs
+      </a>
+      <a href="#" className="page-link text-md text-bold">
+        Shop
+      </a>
+      <a href="#" className="page-link text-md text-bold">
+        Quiz
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
